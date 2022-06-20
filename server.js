d81b2f00e26b31e91513a59ff4c64b5a953eb709
TLAMHutto <keatonhutto8@gmail.com>
2022-06-19 20:52:17 -0500

    helmet

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use(express.json())
-app.use(helmet())
+app.use(helmet(
+  {contentSecurityPolicy: false}
+))
 app.use(xss())
 app.use(mongoSanitize())
 
