afed9e084022353d0a91eb67e73e380668c47ff2
TLAMHutto <keatonhutto8@gmail.com>
2024-04-17 18:55:12 -0500

    web echos

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <Logo />
+        
       </nav>
       <div className='container page'>
         {/* info */}
@@ -15,10 +15,7 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Track your job applications, interviews, and offers in one place.
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
 const connectDB = (url) => {
+
   return mongoose.connect(url)
 }
 
